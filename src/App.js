03799ef47861958e6861491475f091ac0aa213f8
Logan <logankrieger317@gmail.com>
2024-02-12 15:03:06 -0600

    React Router Home,Profile,Chat

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React from 'react';
 import './App.css';
 import './Css/Icons.css'
 import Header from './Components/Header';
+import { BrowserRouter,Router,Route,Routes } from "react-router-dom";
+import Chat from './Components/Chat';
+
 
 
 function App() {
   return (
     <div className="App">
-     
+    <Header/> 
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>Home</div>}/>
+        <Route path='/profile' element={<div>Profile</div>}/>
+        <Route path='/chat'  element={<Chat />}/>
+      </Routes>
+    </BrowserRouter>
 
-      {/* header section*/}
-      <Header/> 
-      {/* Dating card */}
-
-      {/* button menu below */}
-
-      {/* messages screen */}
-
-      {/* individual message */}
 
 
     </div>
@@ -25,3 +26,11 @@ function App() {
 }
 
 export default App;
+
+{/* Dating card */}
+
+{/* button menu below */}
+
+{/* messages screen */}
+
+{/* individual message */}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,14 +3,24 @@ import ForumIcon from '@mui/icons-material/Forum';
 import Person2Icon from '@mui/icons-material/Person2';
 import PetsIcon from '@mui/icons-material/Pets';
 import '../Css/Header.css';
+import { IconButton } from '@mui/material';
 
 function Header() {
   return (
     <div className='header'>
+    
+    <IconButton>
        <Person2Icon id='personIcon'/>
+    </IconButton>
+    
+    <IconButton>
        <PetsIcon id='petsIcon'/>
+    </IconButton>
+    
+    <IconButton>
        <ForumIcon id='messagesIcon'/>
-
+    </IconButton>
+    
     </div>
   )
 }
