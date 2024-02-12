a9b0874612b76e2d839defa56dcfa0ce7eb82ba0
Logan <logankrieger317@gmail.com>
2024-02-12 16:51:30 -0600

    Firebase Config created

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDyVNJ_xPWCjqjjp-mzwYpDhSb7rNiwcQs",
+  authDomain: "barks-and-sparks.firebaseapp.com",
+  projectId: "barks-and-sparks",
+  storageBucket: "barks-and-sparks.appspot.com",
+  messagingSenderId: "327963055604",
+  appId: "1:327963055604:web:da038bc376d86229ecd5a1",
+  measurementId: "G-Q6128R6038"
+};
